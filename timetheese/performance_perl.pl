86754e95cc16c28ef5656ec2dc8171e4157a4caa
hurricup <hurricup@gmail.com>
2015-01-17 16:49:31 +0300

    Regexp testing

--- timetheese/performance_perl.pl
+++ timetheese/performance_perl.pl
@@ -23,32 +23,81 @@ use Benchmark qw(:all);
     # 'Unpack' => sub{ myunpack(1,2,3,1,2,3,'a' => 'b', 'c' => 123, 'd' => [1,2,3]); },
 # });
 
-# # speed of compiled vs inline regexp
-# my $reg = qr/(if|this|is|a|test)/s;
+################### speed of compiled vs inline regexp#########################
 
-# sub precomp_check
-# {
-    # my $arg = shift;
-    # return $arg =~ $reg;
-# }
+my $reg = qr/(if|this|is|a|test)/s;
 
-# sub comp_check
-# {
-    # my $arg = shift;
-    # return $arg =~ /(if|this|is|a|test)/s;
-# }
+sub precomp_check
+{
+    my $arg = shift;
+    return ${$arg} =~ $reg;
+}
 
-# sub precomp_replace
+sub comp_check
+{
+    my $arg = shift;
+    return ${$arg} =~ /(if|this|is|a|test)/s;
+}
+
+sub precomp_replace
+{
+    my $arg = shift;
+    ${$arg} =~ s/$reg//;
+    return $arg;
+}
+
+sub comp_replace
+{
+    my $arg = shift;
+    ${$arg} =~ s/(if|this|is|a|test)//s;
+    return $arg;
+}
+
+my $text = <<'_EOT_' x 1000;
+Hello, this
+is a test message
+for regexp testing
+_EOT_
+
+#die "Update regexp" if precomp_replace() ne comp_replace();
+#warn precomp_replace($text);
+
+my $text1 = $text;
+my $text2 = $text;
+my $text3 = $text;
+my $text4 = $text;
+
+timethese( 1000000, {
+    'Precomp check  ' => sub{ precomp_check(\$text1); },
+    'Comp check     ' => sub{ comp_check(\$text2); },
+});
+
+timethese( 200000, {
+    'Precomp replace' => sub{ precomp_replace(\$text3); },
+    'Comp replace   ' => sub{ comp_replace(\$text4); },
+});
+
+############# scalars tossing vas scalars refs #############
+
+
+# sub val_val
 # {
-    # my $arg = shift;
-    # $arg =~ s/$reg//;
+    # my( $arg ) = (@_);
+    # $arg .= '';
     # return $arg;
 # }
 
-# sub comp_replace
+# sub ref_val
 # {
-    # my $arg = shift;
-    # $arg =~ s/(if|this|is|a|test)//s;
+    # my( $arg ) = (@_);
+    # ${$arg}.='';
+    # return ${$arg};
+# }
+
+# sub ref_ref
+# {
+    # my( $arg ) = (@_);
+    # ${$arg} .= '';
     # return $arg;
 # }
 
@@ -58,58 +107,18 @@ use Benchmark qw(:all);
 # for regexp testing
 # _EOT_
 
-# die "Update regexp" if precomp_replace() ne comp_replace();
-# warn precomp_replace($text);
+# my $suffix = 'b' x 100000;
+# my $test1 = $text.$suffix;
+# my $test2 = $text.$suffix;
+# my $test3 = $text.$suffix;
+# my $test4; 
 
-# timethese( 8000000, {
-    # 'Precomp check  ' => sub{ precomp_check($text); },
-    # 'Comp check     ' => sub{ comp_check($text); },
-    # 'Precomp replace' => sub{ precomp_replace($text); },
-    # 'Comp replace   ' => sub{ comp_replace($text); },
+# timethese( 2000000, {
+    # 'Value-Value' => sub{ $test1 = val_val($test1);},
+    # 'Ref-Value  ' => sub{ $test2 = ref_val(\$test2);},
+    # 'Ref-Ref    ' => sub{ $test4 = ref_ref(\$test3);},
 # });
 
-############# scalars tossing vas scalars refs #############
-
-
-sub val_val
-{
-    my( $arg ) = (@_);
-    $arg .= '';
-    return $arg;
-}
-
-sub ref_val
-{
-    my( $arg ) = (@_);
-    ${$arg}.='';
-    return ${$arg};
-}
-
-sub ref_ref
-{
-    my( $arg ) = (@_);
-    ${$arg} .= '';
-    return $arg;
-}
-
-my $text = <<'_EOT_';
-Hello, this
-is a test message
-for regexp testing
-_EOT_
-
-my $suffix = 'b' x 100000;
-my $test1 = $text.$suffix;
-my $test2 = $text.$suffix;
-my $test3 = $text.$suffix;
-my $test4; 
-
-timethese( 2000000, {
-    'Value-Value' => sub{ $test1 = val_val($test1);},
-    'Ref-Value  ' => sub{ $test2 = ref_val(\$test2);},
-    'Ref-Ref    ' => sub{ $test4 = ref_ref(\$test3);},
-});
-
-die "Something is wrong 1 and 2" if $test1 ne $test2;
-die "Something is wrong 2 and 3" if $test2 ne $test3;
+# die "Something is wrong 1 and 2" if $test1 ne $test2;
+# die "Something is wrong 2 and 3" if $test2 ne $test3;
 
