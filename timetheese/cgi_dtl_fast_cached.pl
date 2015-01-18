bdf3163c52160303195aa20aaf42f5e3a46544ff
hurricup <hurricup@gmail.com>
2015-01-18 15:15:56 +0300

    Tests update

--- timetheese/cgi_dtl_fast_cached.pl
+++ timetheese/cgi_dtl_fast_cached.pl
@@ -10,6 +10,44 @@ my $context = {
     'var4' => 'GREEKS',
     'var5' => 'GO HOME!',
     'array1' => [qw( this is a text string as array )],
+    "results" =>  
+    {
+        "test" =>  
+        {
+            "time_taken" =>  1, 
+            "per_call" =>  1
+        },
+        "test2" =>  
+        {
+            "time_taken" =>  1, 
+            "per_call" =>  1
+        },
+        "test3" =>  
+        {
+            "time_taken" =>  1, 
+            "per_call" =>  1
+        }
+    },
+    "platform" =>  
+    {
+        "django_version" =>  1,
+        "python_version" =>  2
+    },
+    "error_message" =>  "error!",
+    "poll" =>  
+    {
+        "question" =>  "test_question",
+        "choice_set" =>  
+        {
+            "all" =>  
+            [
+                {"id" => 0, "choice_text" => "choice 0"},
+                {"id" => 1, "choice_text" => "choice 1"},
+                {"id" => 2, "choice_text" => "choice 2"},
+                {"id" => 3, "choice_text" => "choice 3"},
+            ]
+        }
+    }
 };
 
 my $cache = DTL::Fast::Cache::File->new('./cache');
@@ -19,5 +57,12 @@ my $tpl = get_template(
 #    'child5.txt',
     , 'dirs' => [ './tpl' ]
     , 'cache' => $cache
+    , 'url_source' => \&uri_source
 );
+
+sub uri_source
+{
+    return '^$';
+}
+
 $tpl->render($context);
