37441b6432c849a88febd6bb3ffa538c95086296
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2017-02-27 12:33:08 +0300

    improved testing docker script, added logging

--- testindocker.pl
+++ testindocker.pl
@@ -4,10 +4,20 @@ use strict;
 use warnings FATAL => 'all';
 use v5.10;
 
-my @images = qw(5.24 5.22 5.20 5.18 5.16 5.14);
+my @images = qw(5.24 5.22 5.20 5.18 5.16 5.14 5.12 5.10);
 
 for my $version (@images)
 {
-    my $imagename = "perl:$version";
-    die "Failed for $imagename" if (system "docker run -it --rm --name perltest -v C:/Repository/DTL-Fast/:/workroot -w /workroot $imagename bash dockertest.sh");
+    my $image_name = "perl:$version";
+    my $log_name = "${image_name}_err.log";
+    $log_name =~ s/[^\w\s\.\-]/#/gsi;
+    say STDERR "Testing with $image_name...";
+    if( system "docker run -it --rm --name perltest -v C:/Repository/DTL-Fast/:/workroot -w /workroot $image_name bash dockertest.sh > $log_name"){
+        say STDERR "FAILED, logged into $log_name";
+    }
+    else
+    {
+        say STDERR "PASSED";
+        unlink $log_name;
+    }
 }
