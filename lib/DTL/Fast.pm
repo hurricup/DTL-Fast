f2b2f82530304525d360119ad958213fdf17f8ac
hurricup <hurricup@gmail.com>
2017-05-08 14:42:30 +0300

    Re-formatted code

--- lib/DTL/Fast.pm
+++ lib/DTL/Fast.pm
@@ -1,5 +1,6 @@
 package DTL::Fast;
-use strict; use warnings FATAL => 'all'; 
+use strict;
+use warnings FATAL => 'all';
 use Exporter 'import';
 use Digest::MD5 qw(md5_hex);
 
@@ -29,29 +30,29 @@ our $RUNTIME_CACHE;
 
 our @EXPORT_OK = qw(
     count_lines
-    get_template
-    select_template
-    register_tag
-    preload_operators
-    register_operator
-    preload_tags
-    register_filter
-    preload_filters
+        get_template
+        select_template
+        register_tag
+        preload_operators
+        register_operator
+        preload_tags
+        register_filter
+        preload_filters
     );
 
 sub get_template
 {
-    my( $template_name, %kwargs ) = @_;
-    
-    die  "Template name was not specified" 
-        if not $template_name;
+    my ( $template_name, %kwargs ) = @_;
+
+    die  "Template name was not specified"
+        if (not $template_name);
 
     die "Template directories array was not specified"
         if
-            not defined $kwargs{'dirs'}
-            or ref $kwargs{'dirs'} ne 'ARRAY'
-            or not scalar @{$kwargs{'dirs'}}
-        ;
+            (not defined $kwargs{dirs}
+                or ref $kwargs{dirs} ne 'ARRAY'
+                or not scalar @{$kwargs{dirs}})
+    ;
 
     my $cache_key = _get_cache_key( $template_name, %kwargs );
 
@@ -59,39 +60,39 @@ sub get_template
 
     $RUNTIME_CACHE //= DTL::Fast::Cache::Runtime->new();
 
-    if( 
-        $kwargs{'no_cache'}
-        or not defined ( $template = $RUNTIME_CACHE->get($cache_key))
+    if (
+        $kwargs{no_cache}
+            or not defined ( $template = $RUNTIME_CACHE->get($cache_key))
     )
     {
         $template = read_template($template_name, %kwargs );
-        
-        if( defined $template )
+
+        if (defined $template)
         {
             $RUNTIME_CACHE->put($cache_key, $template);
         }
         else
         {
-            die  sprintf( <<'_EOT_', $template_name, join("\n", @{$kwargs{'dirs'}}));
+            die  sprintf( <<'_EOT_', $template_name, join("\n", @{$kwargs{dirs}}));
 Unable to find template %s in directories: 
 %s
 _EOT_
         }
     }
-   
+
     return $template;
 }
 
 sub _get_cache_key
 {
-    my( $template_name, %kwargs ) = @_;
-    
+    my ( $template_name, %kwargs ) = @_;
+
     return md5_hex(
         sprintf( '%s:%s:%s:%s'
             , __PACKAGE__
             , $template_name
-            , join( ',', @{$kwargs{'dirs'}} )
-            , join( ',', @{$kwargs{'ssi_dirs'}//[]})
+            , join( ',', @{$kwargs{dirs}} )
+            , join( ',', @{$kwargs{ssi_dirs} // [ ]})
             # shouldn't we pass uri_handler here?
         )
     )
@@ -100,50 +101,50 @@ sub _get_cache_key
 
 sub read_template
 {
-    my( $template_name, %kwargs ) = @_;
-    
+    my ( $template_name, %kwargs ) = @_;
+
     my $template = undef;
     my $template_path = undef;
 
     die "Template directories array was not specified"
-        if not defined $kwargs{'dirs'}
-            or not ref $kwargs{'dirs'}
-            or not scalar @{$kwargs{'dirs'}}
-        ;
-    
+        if (not defined $kwargs{dirs}
+            or not ref $kwargs{dirs}
+            or not scalar @{$kwargs{dirs}})
+    ;
+
     my $cache_key = _get_cache_key( $template_name, %kwargs );
 
-    if( 
-        $kwargs{'no_cache'}
-        or not exists $kwargs{'cache'} 
-        or not $kwargs{'cache'}
-        or not $kwargs{'cache'}->isa('DTL::Fast::Cache')
-        or not defined ($template = $kwargs{'cache'}->get($cache_key))
+    if (
+        $kwargs{no_cache}
+            or not exists $kwargs{cache}
+            or not $kwargs{cache}
+            or not $kwargs{cache}->isa('DTL::Fast::Cache')
+            or not defined ($template = $kwargs{cache}->get($cache_key))
     )
     {
-        ($template, $template_path) = _read_file($template_name, $kwargs{'dirs'});
-        
-        if( defined $template )
+        ($template, $template_path) = _read_file($template_name, $kwargs{dirs});
+
+        if (defined $template)
         {
-            $kwargs{'file_path'} = $template_path;
+            $kwargs{file_path} = $template_path;
             $template = DTL::Fast::Template->new( $template, %kwargs);
-    
-            $kwargs{'cache'}->put( $cache_key, $template )
-                if 
-                    defined $template
-                    and exists $kwargs{'cache'}
-                    and $kwargs{'cache'}
-                    and $kwargs{'cache'}->isa('DTL::Fast::Cache')
-                ;
+
+            $kwargs{cache}->put( $cache_key, $template )
+                if
+                    (defined $template
+                        and exists $kwargs{cache}
+                        and $kwargs{cache}
+                        and $kwargs{cache}->isa('DTL::Fast::Cache'))
+            ;
         }
     }
-    
-    if( defined $template )
+
+    if (defined $template)
     {
-        $template->{'cache'} = $kwargs{'cache'} if $kwargs{'cache'};
-        $template->{'url_source'} = $kwargs{'url_source'} if $kwargs{'url_source'};
+        $template->{cache} = $kwargs{cache} if ($kwargs{cache});
+        $template->{url_source} = $kwargs{url_source} if ($kwargs{url_source});
     }
-    
+
     return $template;
 }
 
@@ -153,32 +154,32 @@ sub _read_file
     my $dirs = shift;
     my $template;
     my $template_path;
-    
+
     foreach my $dir (@$dirs)
     {
         $dir =~ s/[\/\\]+$//xgsi;
         $template_path = sprintf '%s/%s', $dir, $template_name;
-        if( 
+        if (
             -e $template_path
-            and -f $template_path
-            and -r $template_path
+                and -f $template_path
+                and -r $template_path
         )
         {
             $template = __read_file( $template_path );
             last;
-        }        
+        }
     }
-    
+
     return ($template, $template_path);
 }
 
 
 sub __read_file
 {
-    my( $file_name ) = @_;
+    my ( $file_name ) = @_;
     my $result;
-    
-    if( open my $IF, '<', $file_name )
+
+    if (open my $IF, '<', $file_name)
     {
         local $/ = undef;
         $result = <$IF>;
@@ -187,10 +188,10 @@ sub __read_file
     else
     {
         die  sprintf(
-            'Error opening file %s, %s'
-            , $file_name
-            , $!
-        );
+                'Error opening file %s, %s'
+                , $file_name
+                , $!
+            );
     }
     return $result;
 }
@@ -198,39 +199,39 @@ sub __read_file
 # result should be cached with full list of params
 sub select_template
 {
-    my( $template_names, %kwargs ) = @_;
-    
-    die  "First parameter must be a template names array reference" 
-        if(
+    my ( $template_names, %kwargs ) = @_;
+
+    die  "First parameter must be a template names array reference"
+        if (
             not ref $template_names
-            or ref $template_names ne 'ARRAY'
-            or not scalar @$template_names
+                or ref $template_names ne 'ARRAY'
+                or not scalar @$template_names
         );
 
     my $result = undef;
-    
+
     foreach my $template_name (@$template_names)
     {
-        if( ref ( $result = get_template( $template_name, %kwargs )) eq 'DTL::Fast::Template' )
+        if (ref ( $result = get_template( $template_name, %kwargs )) eq 'DTL::Fast::Template')
         {
             last;
         }
     }
-    
+
     return $result;
 }
 
 # registering tag as known
 sub register_tag
 {
-    my( %tags ) = @_;
-    
-    while( my( $slug, $module) = each %tags )
+    my ( %tags ) = @_;
+
+    while( my ( $slug, $module) = each %tags )
     {
         $DTL::Fast::KNOWN_TAGS{lc($slug)} = $module;
         $DTL::Fast::KNOWN_SLUGS{$module} = $slug;
     }
-    
+
     return;
 }
 
@@ -238,14 +239,14 @@ sub register_tag
 sub preload_tags
 {
     require Module::Load;
-    
-    while( my( $keyword, $module) = each %KNOWN_TAGS )
+
+    while( my ( $keyword, $module) = each %KNOWN_TAGS )
     {
         Module::Load::load($module);
         $LOADED_MODULES{$module} = time;
-        delete $TAG_HANDLERS{$keyword} if exists $TAG_HANDLERS{$keyword} and $TAG_HANDLERS{$keyword} ne $module;
+        delete $TAG_HANDLERS{$keyword} if (exists $TAG_HANDLERS{$keyword} and $TAG_HANDLERS{$keyword} ne $module);
     }
-    
+
     return 1;
 }
 
@@ -253,27 +254,27 @@ sub preload_tags
 # registering filter as known
 sub register_filter
 {
-    my( %filters ) = @_;
-    
-    while( my( $slug, $module) = each %filters )
+    my ( %filters ) = @_;
+
+    while( my ( $slug, $module) = each %filters )
     {
         $DTL::Fast::KNOWN_FILTERS{$slug} = $module;
-        delete $FILTER_HANDLERS{$slug} if exists $FILTER_HANDLERS{$slug} and $FILTER_HANDLERS{$slug} ne $module;
+        delete $FILTER_HANDLERS{$slug} if (exists $FILTER_HANDLERS{$slug} and $FILTER_HANDLERS{$slug} ne $module);
     }
-    
+
     return;
 }
 
 sub preload_filters
 {
     require Module::Load;
-    
-    while( my( undef, $module) = each %KNOWN_FILTERS )
+
+    while( my ( undef, $module) = each %KNOWN_FILTERS )
     {
         Module::Load::load($module);
         $LOADED_MODULES{$module} = time;
     }
-    
+
     return 1;
 }
 
@@ -284,22 +285,22 @@ sub preload_filters
 sub register_operator
 {
     my %ops = @_;
-    
+
     my %recompile = ();
     foreach my $operator (keys %ops)
     {
-        my($priority, $module) = @{$ops{$operator}};
-        
+        my ($priority, $module) = @{$ops{$operator}};
+
         die "Operator priority must be a number from 0 to 8"
-            if $priority !~ /^[012345678]$/;
-        
-        $KNOWN_OPS{$priority} //= {};
+            if ($priority !~ /^[012345678]$/);
+
+        $KNOWN_OPS{$priority} //= { };
         $KNOWN_OPS{$priority}->{$operator} = $module;
         $recompile{$priority} = 1;
         $KNOWN_OPS_PLAIN{$operator} = $module;
-        delete $OPS_HANDLERS{$operator} if exists $OPS_HANDLERS{$operator} and $OPS_HANDLERS{$operator} ne $module;
+        delete $OPS_HANDLERS{$operator} if (exists $OPS_HANDLERS{$operator} and $OPS_HANDLERS{$operator} ne $module);
     }
-    
+
     foreach my $priority (keys(%recompile))
     {
         my @ops = sort{ length $b <=> length $a } keys(%{$KNOWN_OPS{$priority}});
@@ -312,13 +313,13 @@ sub register_operator
 sub preload_operators
 {
     require Module::Load;
-    
-    while( my( undef, $module) = each %KNOWN_OPS_PLAIN )
+
+    while( my ( undef, $module) = each %KNOWN_OPS_PLAIN )
     {
         Module::Load::load($module);
         $LOADED_MODULES{$module} = time;
     }
-    
+
     return 1;
 }
 
